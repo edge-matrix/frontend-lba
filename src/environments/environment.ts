7d1dc7d1a673459dcc1c5bc9cb0432e6685b5be2
rishi pastor <rishipastor1607@gmail.com>
2025-02-02 04:01:33 +0530

    new-changes to home footer and menu page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,20 +5,21 @@
 export const environment = {
   production: false,
   appUrl: 'http://localhost:4200/',
-  baseUrl: 'http://127.0.0.1:8000/api',
-  storage: 'http://127.0.0.1:8000/storage/',
+  baseUrl: 'https://dev.letsbunkagain.com/backend/api',
+  storage: 'https://dev.letsbunkagain.com/backend/storage/',
   redirectUrl: 'http://localhost:4200/login',
   GoogleCaptchaKey: '6LftGB4jAAAAAGLknjW4C5iBBPxcclUv6BfhXm6J',
-  Google_client_id: '997443453271-0cqbcm3icjuglbl8cg7nld72jqq40suv.apps.googleusercontent.com',
+  Google_client_id:
+    '997443453271-0cqbcm3icjuglbl8cg7nld72jqq40suv.apps.googleusercontent.com',
   Google_map_api: 'AIzaSyBcGxO02ZpNo6DHzALYUToWpTXolnmrO14',
   firebaseConfig: {
-    apiKey: "AIzaSyA8ZWTbb3M4uWImzkd6ToE4Vj5P-aNQ3To",
-    authDomain: "lets-bunk-again-com.firebaseapp.com",
-    projectId: "lets-bunk-again-com",
-    storageBucket: "lets-bunk-again-com.appspot.com",
-    messagingSenderId: "347467439723",
-    appId: "1:347467439723:web:3d9fbf8a128b56b55d1b2e",
-    measurementId: "G-WCQ5LJLX8S"
+    apiKey: 'AIzaSyA8ZWTbb3M4uWImzkd6ToE4Vj5P-aNQ3To',
+    authDomain: 'lets-bunk-again-com.firebaseapp.com',
+    projectId: 'lets-bunk-again-com',
+    storageBucket: 'lets-bunk-again-com.appspot.com',
+    messagingSenderId: '347467439723',
+    appId: '1:347467439723:web:3d9fbf8a128b56b55d1b2e',
+    measurementId: 'G-WCQ5LJLX8S',
   },
   // paymentUrl: 'https://mercury-t2.phonepe.com/transact/pg',   //Phone Pay Prod
   // paymentUrl: 'https://mercury-uat.phonepe.com/transact/simulator', //Phone Pay Demo
@@ -27,7 +28,7 @@ export const environment = {
   pusher: '5bdb30b6e88270a5117a',
 
   EncryptKey: 'bbZMZJWf90AhF9tOeJSQ0XOrIaFlsB8b4G1BLflovY4=',
-  EncryptIV: 'AES-256-CBC'
+  EncryptIV: 'AES-256-CBC',
 };
 
 /*
